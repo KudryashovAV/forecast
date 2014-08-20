8d18031db72f6a5eeae6cc615f729771b3918df9
Anton Ilin <anton@ilin.dn.ua>
2014-08-20 13:56:27 +0300

    change HTTParty

--- app/models/open_weather.rb
+++ app/models/open_weather.rb
@@ -1,9 +1,6 @@
 class OpenWeather < ActiveRecord::Base
-  include HTTParty
-  base_uri "http://api.openweathermap.org"
-  format :json
-
+  
   def current(query)
-    self.class.get('/data/2.5/weather', q: query)
+    HTTParty.get('http://api.openweathermap.org/data/2.5/weather', :q => query)
   end
 end
